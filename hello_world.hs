6c18515483657853cb55c21cd59fb887da840c29
haskellcg <13324558170@163.com>
2019-01-28 00:35:23 +0800

    test commit

--- hello_world.hs
+++ hello_world.hs
@@ -4,4 +4,4 @@ haskellcg_putStrLn :: String -> IO ()
 haskellcg_putStrLn xs = foldr (>>) done (map putChar xs) >> 
                         putChar '\n'
 
-main = putStrLn "Hello World!" >> putChar 'X' >> putChar '\n' >> haskellcg_putStrLn "my println function"
+main = putStrLn "Hello World!" >> putChar 'X' >> putChar '\n' >> haskellcg_putStrLn "My Print Function"
