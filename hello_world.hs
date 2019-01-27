76ec17d53da88b5d128abe141dc1c1a3a88a3706
haskellcg <13324558170@163.com>
2019-01-28 00:22:11 +0800

    add println function

--- hello_world.hs
+++ hello_world.hs
@@ -1,1 +1,7 @@
-main = putStrLn "Hello World!"
+done = return ()
+
+haskellcg_putStrLn :: String -> IO ()
+haskellcg_putStrLn xs = foldr (>>) done (map putChar xs) >> 
+                        putChar '\n'
+
+main = putStrLn "Hello World!" >> putChar 'X' >> putChar '\n' >> haskellcg_putStrLn "my println function"
