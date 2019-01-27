232bf62cd5015b070b60cd30633a9cf1fadff62c
haskellcg <13324558170@163.com>
2019-01-28 01:08:36 +0800

    add readLn

--- hello_world.hs
+++ hello_world.hs
@@ -4,4 +4,11 @@ haskellcg_putStrLn :: String -> IO ()
 haskellcg_putStrLn xs = foldr (>>) done (map putChar xs) >> 
                         putChar '\n'
 
-main = putStrLn "Hello World!" >> putChar 'X' >> putChar '\n' >> haskellcg_putStrLn "My Print Function"
+main = do putStrLn "Hello World!"
+          putChar 'X'
+          putChar '\n'
+          haskellcg_putStrLn "My Print Function"
+          x <- readLn
+          if x == 4 
+            then putStrLn "world"
+            else putStrLn "wrong"
